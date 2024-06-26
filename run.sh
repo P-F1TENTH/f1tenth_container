a6e22a301486820a08615eb2bc83820f0fc8830d
Ignacy Kajdan <ignacy.kajdan@gmail.com>
2024-06-26 14:13:48 +0200

    Pass X11 socket to the container

--- run.sh
+++ run.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
+set -eu
+
+NAME=f1tenth_docking_ros
+IMAGE=f1tenth_docking_ros
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+
 if ! [ -d "src" ]; then
 	echo "Error: directory `src` not found. Please run this script from the root of the f1tenth_docking repository."
 	exit 1
 fi
 
-docker run \
-  -it \
-  --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --privileged \
-  --network=host \
-  --device=/dev/ttyACM0 \
-  --device=/dev/ttyUSB0 \
-  --device=/dev/ttyUSB1 \
-  --device=/dev/ttyUSB2 \
-  --device=/dev/input/js0 \
-  --volume "${PWD}"/src:/root/ws/src \
-  --volume "${PWD}"/launch:/root/ws/launch \
-  --name=f1tenth_docking_ros \
-  f1tenth_docking_ros \
-  bash
+if ! [ -f "${XAUTH}" ]; then
+  touch "${XAUTH}"
+  xauth nlist "${DISPLAY}" | sed -e 's/^..../ffff/' | xauth -f "${XAUTH}" nmerge -
+  chmod 644 "${XAUTH}"
+fi
+
+if [ "$(docker ps -aq -f status=exited -f name="${NAME}")" ]; then
+  # Start the existing container and attach to it
+  docker start "${NAME}"
+  exec docker exec -it "${NAME}" bash
+elif [ "$(docker ps -aq -f status=running -f name="${NAME}")" ]; then
+  # Attach to the running container
+  exec docker exec -it "${NAME}" bash
+else
+  # Create a new container and attach to it
+  exec docker run \
+    -it \
+    --env=DISPLAY="${DISPLAY}"  \
+    --env=QT_X11_NO_MITSHM=1 \
+    --env=XDG_RUNTIME_DIR=/tmp \
+    --env=WAYLAND_DISPLAY="${WAYLAND_DISPLAY}" \
+    --volume="${XDG_RUNTIME_DIR}"/"${WAYLAND_DISPLAY}":/tmp/"${WAYLAND_DISPLAY}" \
+    --env=XAUTHORITY="${XAUTH}" \
+    --volume="${XAUTH}":"${XAUTH}" \
+    --volume="${XSOCK}":"${XSOCK}" \
+    --volume "${PWD}"/src:/root/ws/src \
+    --volume "${PWD}"/launch:/root/ws/launch \
+    --network=host \
+    --name="${NAME}" \
+    "${IMAGE}" \
+    bash
+fi
